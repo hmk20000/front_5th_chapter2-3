56fac85006d104f65c81f7ab182a5c8dbf9f4323
codeming <hmk2000015@gmail.com>
2025-05-01 18:40:27 +0900

    feature: vite 빌드 시점에 경로 변환 처리

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'api-replace',
+      transform(code, id) {
+        if (id.includes('api')) {
+          return code.replace(/\/api/g, 'https://dummyjson.com');
+        }
+      },
+    },
+  ],
   base: '/front_5th_chapter2-3',
   build: {
     outDir: 'docs',
